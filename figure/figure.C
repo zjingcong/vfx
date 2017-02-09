76828d5c0e3de21117f009edfaa703455552b3ae
Jingcong Zhang <jingcoz@titan2.cs.clemson.edu>
2017-02-09 13:19:43 -0500

    Multi-threads and turntable.

--- figure/figure.C
+++ figure/figure.C
@@ -9,6 +9,8 @@
 
 # include <iostream>
 # include <stdlib.h>
+# include <math.h>
+# include <stdio.h>
 
 using namespace lux;
 using namespace std;
@@ -16,6 +18,18 @@ using namespace std;
 # define STEP_SIZE 0.3
 # define WEIGHT 320
 # define HEIGHT 180
+# define FAR 10
+# define FRAME_NUM 4
+# define PI 3.14159265
+
+
+// theta in degree
+// axis should be unit vector
+Vector vecRotation(Vector v, Vector axis, float a)
+{
+	float theta = PI * a / 180.0;
+	return v * cos(theta) + axis * (axis * v) * ( 1 - cos(theta)) + (axis ^ v) * sin(theta);
+}
 
 
 // reference: basicShape & Xform
@@ -35,6 +49,7 @@ using namespace std;
 	// ImplicitBlinnBlend testVolume(testVolume2, testVolume3);
 
 
+// ===================================== setup ========================================
 // volume setup
 Sphere volume1(1.0);
 Box box(2.0, 2);
@@ -54,52 +69,45 @@ DensityVolume volume2Density(constantDensity, volume2);
 ColorAdd finalColor(volume1Color, volume2Color);
 FloatAdd finalDensity(volume1Density, volume2Density);
 
-// camera setup
-Camera myCamera;
-
-// frame setup
-Image myImg;
+Vector originEye(10.0, 0.0, 0.0);
 
 // ------------------------------------------------------------------------------------
 
 
-// turntable of 360 degrees/120 frames
-// start from xc at x-axis at frame-0
-Vector turntable(int frame_id, float distance)
-{
-  Vector c0(distance, 0.0, 0.0);
-  return
-}
-
-
-void renderSetup()
-{
-  myCamera.setFarPlane(20.0);
-  myImg.reset(WEIGHT, HEIGHT);
-}
-
-
-// turntable
-void frameRender(int frame_id)
-{
-  
-}
-
-
 int main()
 {
-  renderSetup();
+	cout << "Rendering start..." << endl;
+	# pragma omp parallel for
+	for (int frame_id = 0; frame_id < FRAME_NUM; ++frame_id)
+	{
+		Image myImg;
+		myImg.reset(WEIGHT, HEIGHT);
 
-	Vector eye(10.0, 0.0, 0.0);
-	Vector view(-1.0, 0.0, 0.0);  // camera view will set to unitvector in Camera.C
-	Vector up(0.0, 1.0, 0.0);
-	myCamera.setEyeViewUp(eye, view, up);
-  Renderer myRenderer(myImg, myCamera, STEP_SIZE);
-	// rendering
-	myRenderer.render(finalVolume, finalColor, finalDensity);
-	// write into file
-	cout << "Write exr image into " << "test.exr" << endl;
-	writeOIIOImage("test.exr", myImg);
+		Camera myCamera;
+		myCamera.setFarPlane(FAR);
+
+		float angle = float(360) / FRAME_NUM;
+		float theta = frame_id  * angle;
+		Vector up(0.0, 1.0, 0.0);
+		Vector lookAt(0.0, 0.0, 0.0);
+		Vector camera_axis(0.0, 1.0, 0.0);
+
+		// get camera position and camera view
+		Vector eye_new = vecRotation(originEye, camera_axis, theta);
+		Vector view_new = lookAt - eye_new;
+		myCamera.setEyeViewUp(eye_new, view_new, up);
+
+		// rendering
+		Renderer myRenderer(myImg, myCamera, STEP_SIZE);
+		myRenderer.render(finalVolume, finalColor, finalDensity);
+
+		// write into file
+		char file_name[50];
+		sprintf(file_name, "./result/jingcoz_hw1.%04d.exr", frame_id);
+		cout << "Render frame " << file_name << "..."<< endl;
+		writeOIIOImage(file_name, myImg);
+	}
+	cout << "Rendering complete!" << endl;
 
 	return 0;
 }
