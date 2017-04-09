3a499396ec4d4690e121b0b77ad834af37c30708
zjingcong <zhjingcong@gmail.com>
2017-04-09 12:46:25 -0400

    Add advection noise config.

---
+++ advect/include/ConfigParser.h
@@ -0,0 +1,49 @@
+# ifndef __CONFIGPARSER_H__
+# define __CONFIGPARSER_H__
+
+# include <vector>
+# include <map>
+# include <iostream>
+# include <fstream>
+# include <string>
+
+using namespace std;
+
+
+namespace cfg
+{
+    typedef map<string, float> FloatValueMap;
+
+    FloatValueMap floatValueParser(string cfgFileName)
+    {
+        ifstream cfgFile(cfgFileName, std::ios::in);
+        if (!cfgFile.is_open())
+        {
+            cout << "Could not read file " << cfgFileName << ". File does not exist." << endl;
+            exit(0);
+        }
+
+        string line = "";
+        FloatValueMap floatValueMap;
+        cout << "Config Value" << endl;
+        cout << "========================" << endl;
+        while (getline(cfgFile, line))
+        {
+            istringstream iss(line);
+            string key;
+            float value;
+            if (iss >> key >> value)
+            {
+                floatValueMap[key] = value;
+                cout << key << ": " << value << endl;
+            }
+        }
+        cout << "========================" << endl;
+        cfgFile.close();
+
+        return floatValueMap;
+    }
+
+}
+
+# endif
