eeaa286b199bd91876bd0898c1028f0e193a9f01
zjingcong <zhjingcong@gmail.com>
2017-04-25 18:24:01 -0400

    Modify wisp parms.

---
+++ wand/include/Wisp.h
@@ -0,0 +1,58 @@
+# ifndef __WISP_H__
+# define __WISP_H__
+
+# include "Types.h"
+# include "Noise.h"
+# include "PerlinNoise.h"
+
+using namespace lux;
+
+
+// wisp parms
+struct WispParms
+{
+    WispParms():
+        corr(0.0),
+        opacity(0.05),
+        dscale(2.0),
+        clump(0.0),
+        delta_x(5),
+        dot_num(5000000)
+        {}
+
+    float corr;
+    float opacity;
+    float dscale;
+    float clump;
+    float delta_x;
+    int dot_num;
+    Noise* FSPN2;
+};
+
+
+class SingleGuideWisp
+{
+    public:
+        // 5 million: 5000000
+        SingleGuideWisp(Noise& n, WispParms& w, float s);
+        ~SingleGuideWisp() {}
+
+        FloatGrid::Ptr getWispGrid()    {return myGrid;}
+        BBox getBBox()  {return wispBBox;}
+
+    private:
+        Noise& guideParticle;   // guide particle
+        WispParms& wisp_parms;
+        float voxelSize;
+        int dot_num;
+
+        FloatGrid::Ptr myGrid;
+        Transform::Ptr xform;
+        BBox wispBBox;
+
+        void createDot();
+        void burnDot(Vector xw);
+        void createWispDots();
+};
+
+# endif
